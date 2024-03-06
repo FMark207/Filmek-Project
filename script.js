9ded05987ee6f4846e082be70f664f964e130a05
FMark207 <mark.farkas.207@gmail.com>
2024-03-06 23:07:14 +0100

    Merged With Goody's Code

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let data = {username: "", password: ""}
 let username = document.getElementById("username")
 let password = document.getElementById("password")
-
-const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-fetch('https://api.themoviedb.org/3/authentication', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
 let storedData = JSON.parse(localStorage.getItem("UserData"))
+
+function logOut() {
+    localStorage.clear()
+}
 
 let deleteIcon = document.getElementById("delete")
 let searchBar = document.getElementById("search-bar")
 
 
+if (deleteIcon && searchBar) {
+    setInterval(() => {
+        if (searchBar.value.length > 0) {
+            deleteIcon.style.display = "block"
+        }
+        else{
+            deleteIcon.style.display = "none"
+        }
+    }, 10);
+}
 
-setInterval(() => {
-    if (searchBar.value.length > 0) {
-        deleteIcon.style.display = "block"
-    }
-    else{
-        deleteIcon.style.display = "none"
-    }
-}, 10);
 
 function deleteText(inputField) {
     inputField.value = ""
